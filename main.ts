a2388a6354993d2ce3d53c0359fab6d33070d849
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2020-05-23 19:25:14 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+let vLight = 0
+let sensors: number[] = []
+let vTemp = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(vLight)
+})
+radio.onReceivedString(function (receivedString) {
+    sensors = ADTools.unpackNums(receivedString, Delimiters.Pipe)
+    vLight = sensors[0]
+    vTemp = sensors[1]
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(vTemp)
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString(ADTools.packNums([input.lightLevel(), input.temperature()], Delimiters.Pipe))
+})
