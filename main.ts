6b7507a314b41f418563280b2ec7a378368296d1
Alexandre Dumont aka Adlx <adumont@users.noreply.github.com>
2020-05-23 19:27:01 +0200

    add example of logNumber()

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ let sensors: number[] = []
 let vTemp = 0
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(vLight)
+    ADTools.logNumber(vLight)
 })
 radio.onReceivedString(function (receivedString) {
     sensors = ADTools.unpackNums(receivedString, Delimiters.Pipe)
@@ -11,6 +12,7 @@ radio.onReceivedString(function (receivedString) {
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(vTemp)
+    ADTools.logNumber(vTemp)
 })
 input.onGesture(Gesture.Shake, function () {
     radio.sendString(ADTools.packNums([input.lightLevel(), input.temperature()], Delimiters.Pipe))
